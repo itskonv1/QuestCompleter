9b9aaf32febbe5037710122d41fd177fd973645e
itskonv1 <yousexv1@gmail.com>
2026-07-05 18:01:32 +0100

    fix: remove server list shortcut button and fix progressTextAnimation patch

--- index.tsx
+++ index.tsx
@@ -6,11 +6,8 @@
 
 import "./styles.css";
 
-import { addServerListElement, removeServerListElement, ServerListRenderPosition } from "@api/ServerList";
-import ErrorBoundary from "@components/ErrorBoundary";
 import definePlugin from "@utils/types";
-import { findComponentByCodeLazy } from "@webpack";
-import { NavigationRouter, QuestStore,React } from "@webpack/common";
+import { QuestStore } from "@webpack/common";
 
 import { QuestOrchestrator } from "./completion";
 import {
@@ -25,36 +22,6 @@ import { settings } from "./settings";
 
 let orchestrator: QuestOrchestrator | null = null;
 let updateTimeoutId: ReturnType<typeof setTimeout> | undefined;
-
-const GuildlessServerListItemComponent = findComponentByCodeLazy("asContainer", "upperBadge");
-
-function QuestIcon({ height, width, className }: { height: number; width: number; className?: string; }): React.JSX.Element {
-    return (
-        <svg viewBox="0 0 24 24" height={height} width={width} fill="none" className={className}>
-            <path fill="currentColor" d="M7.5 21.7a8.95 8.95 0 0 1 9 0 1 1 0 0 0 1-1.73c-.6-.35-1.24-.64-1.9-.87.54-.3 1.05-.65 1.52-1.07a3.98 3.98 0 0 0 5.49-1.8.77.77 0 0 0-.24-.95 3.98 3.98 0 0 0-2.02-.76A4 4 0 0 0 23 10.47a.76.76 0 0 0-.71-.71 4.06 4.06 0 0 0-1.6.22 3.99 3.99 0 0 0 .54-5.35.77.77 0 0 0-.95-.24c-.75.36-1.37.95-1.77 1.67V6a4 4 0 0 0-4.9-3.9.77.77 0 0 0-.6.72 4 4 0 0 0 3.7 4.17c.89 1.3 1.3 2.95 1.3 4.51 0 3.66-2.75 6.5-6 6.5s-6-2.84-6-6.5c0-1.56.41-3.21 1.3-4.51A4 4 0 0 0 11 2.82a.77.77 0 0 0-.6-.72 4.01 4.01 0 0 0-4.9 3.96A4.02 4.02 0 0 0 3.73 4.4a.77.77 0 0 0-.95.24 3.98 3.98 0 0 0 .55 5.35 4 4 0 0 0-1.6-.22.76.76 0 0 0-.72.71l-.01.28a4 4 0 0 0 2.65 3.77c-.75.06-1.45.33-2.02.76-.3.22-.4.62-.24.95a4 4 0 0 0 5.49 1.8c.47.42.98.78 1.53 1.07-.67.23-1.3.52-1.91.87a1 1 0 1 0 1 1.73Z" />
-        </svg>
-    );
-}
-
-function QuestButtonRenderer(): React.JSX.Element {
-    const handleQuestClick = (e: React.MouseEvent) => {
-        if ((e.nativeEvent as MouseEvent).type === "mousedown" && (e.nativeEvent as MouseEvent).button !== 0) return;
-        e.preventDefault();
-        e.stopPropagation();
-        NavigationRouter.transitionTo("/quest-home");
-    };
-
-    return (
-        <div style={{ display: "flex", justifyContent: "center", marginBottom: "8px" }}>
-            <GuildlessServerListItemComponent
-                icon={() => <QuestIcon height={26} width={26} />}
-                tooltip="Quests"
-                onClick={handleQuestClick}
-                onMouseDown={handleQuestClick}
-            />
-        </div>
-    );
-}
 
 function handleQuestsUpdate(): void {
     if (!orchestrator) return;
@@ -93,10 +60,7 @@ export default definePlugin({
         QUEST_CLAIM: debouncedUpdate
     },
 
-    renderQuestifyButton: ErrorBoundary.wrap(QuestButtonRenderer, { noop: true }),
-
     start() {
-        addServerListElement(ServerListRenderPosition.Above, this.renderQuestifyButton);
         orchestrator = new QuestOrchestrator();
         debouncedUpdate();
     },
@@ -142,7 +106,7 @@ export default definePlugin({
         {
             find: ",{progressTextAnimation:",
             replacement: {
-                match: /(let{percentComplete:.{0,115}?children:\i}=)(\i)/,
+                match: /(let{percentComplete:.{0,115}?children:\i,useAltStyle:\i=!1}=)(\i)/,
                 replace: "const questifyProgress=$self.getQuestPanelPercentComplete({...$2,quest:$2.children?.props?.quest});$1Object.assign({},$2,questifyProgress??{})"
             }
         },
@@ -165,7 +129,6 @@ export default definePlugin({
     stop() {
         clearTimeout(updateTimeoutId);
         updateTimeoutId = undefined;
-        removeServerListElement(ServerListRenderPosition.Above, this.renderQuestifyButton);
         if (orchestrator) {
             orchestrator.stopAll();
             orchestrator = null;
